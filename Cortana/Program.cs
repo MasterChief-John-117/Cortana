e9960635cabb09b52a9531516aaf082f17303901
MasterChief-John-117 <galenguyer@outlook.com>
2017-07-02 23:43:51 -0700

    Login + bad token handling

--- Cortana/Program.cs
+++ Cortana/Program.cs
@@ -1,11 +1,90 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Discord;
+using Discord.Commands;
+using Discord.Net;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Cortana
 {
-    internal class Program
+    public class Program
     {
-        public static void Main(string[] args)
+        public static DiscordSocketClient Client;
+        private Configuration _config;
+
+        public static void Main(string[] args) =>
+             new Program().Start().GetAwaiter().GetResult();
+        
+
+
+        public async Task Start()
         {
+            //Make sure we have valid files
+            if (!Directory.Exists("files")) Directory.CreateDirectory("files");
+            if (!File.Exists("files/config.json"))
+            {
+                _config = new Configuration(new ConfigurationBuilder().createConfiguration());
+                File.WriteAllText("files/config.json", JsonConvert.SerializeObject(_config, Formatting.Indented));
+            }
+            else _config = JsonConvert.DeserializeObject<Configuration>(File.ReadAllText("files/config.json"));
+            
+            Client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose, //for most debug, Verbose. For normal use, Crit is fine
+                AlwaysDownloadUsers = true
+            });
+            Client.Log += Log;
+
+            try
+            {
+                await Client.LoginAsync(_config.TokenType, _config.Token);
+                await Client.StartAsync();
+            }
+            catch (Discord.Net.HttpException httpException)
+            {
+                if (httpException.HttpCode == HttpStatusCode.Unauthorized)
+                {
+                    _config.UpdateToken();
+                    Start().GetAwaiter().GetResult();
+                    return;
+                }
+            }
+
+            Client.Ready += _onReady;
+
+            var serviceProvider = ConfigureServices();
+
+//          _handler = new CommandHandler();
+//          await _handler.Install(serviceProvider);
+
+            // Block this program until it is closed.
+            await Task.Delay(-1);
+        }
+        
+        private IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection()
+                .AddSingleton(Client)
+                .AddSingleton(new CommandService(new CommandServiceConfig { CaseSensitiveCommands = false, ThrowOnError = false}));
+            var provider = new DefaultServiceProviderFactory().CreateServiceProvider(services);
+            return provider;
+        }
+
+        private async Task _onReady()
+        {
+            Console.WriteLine("Almost ready to go! Your prefix is \"{0}\"", _config.Prefix);
+            Console.WriteLine($"on {Client.Guilds.Count} guilds");
+
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(DateTime.Now + ": " + msg.ToString());
+            return Task.FromResult(0);
         }
     }
 }
