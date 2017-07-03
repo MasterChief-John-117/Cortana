a5e85ae62d231095685347e6f3acd6ff42e16601
MasterChief-John-117 <galenguyer@outlook.com>
2017-07-03 10:03:39 -0700

    ping command

---
+++ Cortana/Modules/BotModule.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Cortana.Modules
+{
+    public class BotModule : ModuleBase
+    {
+        [Command("ping")][Summary("Gets the time to Discord's servers")]
+        public async Task Ping()
+        {
+            var watch = Stopwatch.StartNew();
+            await Context.Message.ModifyAsync(msg => msg.Content = "͏");
+            watch.Stop();
+            await Context.Message.ModifyAsync(msg => msg.Embed = new EmbedBuilder().WithDescription($":ping_pong: Ping took `{watch.ElapsedMilliseconds}`ms").Build());
+        }
+    }
+}
