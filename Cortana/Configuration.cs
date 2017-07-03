ba843bbe7a8366bf0695cfae9eec8e7ceb6c33dd
MasterChief-John-117 <galenguyer@outlook.com>
2017-07-02 20:30:45 -0700

    Configuration Class
    Config class with JSON support

---
+++ Cortana/Configuration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Discord;
+using Newtonsoft.Json;
+
+namespace Cortana
+{
+    class Configuration
+    {
+        [JsonProperty("token")]
+        public string Token;
+        [JsonProperty("tokenType")]
+        public TokenType TokenType = TokenType.User;
+        [JsonProperty("prefix")]
+        public string Prefix;
+        [JsonProperty("executeEdits")]
+        public bool execEdits;
+
+        public Configuration(string token, TokenType tType, string prefix, bool edits)
+        {
+            this.Token = token;
+            this.TokenType = tType;
+            this.Prefix = prefix;
+            this.execEdits = edits;
+        }
+
+        public Configuration()
+        {
+            Configuration config = JsonConvert.DeserializeObject<Configuration>(File.ReadAllText("files/config.json"));
+            this.Token = config.Token;
+            this.TokenType = config.TokenType;
+            this.Prefix = config.Prefix;
+            this.execEdits = config.execEdits;
+        }
+
+
+    }
+}
