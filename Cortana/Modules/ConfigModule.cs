1259949211e653a100e855403362394d6fc344ad
MasterChief-John-117 <galenguyer@outlook.com>
2017-07-03 21:51:54 -0700

    Config module

---
+++ Cortana/Modules/ConfigModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Newtonsoft.Json;
+
+namespace Cortana.Modules
+{
+    [Group("config")]
+    public class ConfigModule : ModuleBase
+    {
+        [Command("prefix")][Summary("Change the prefix used")][Remarks("config")]
+        public async Task Prefix(string pref)
+        {
+            CommandHandler._config.Prefix = pref;
+            File.WriteAllText("files/config.json", JsonConvert.SerializeObject(CommandHandler._config, Formatting.Indented));
+        }
+    }
+}
