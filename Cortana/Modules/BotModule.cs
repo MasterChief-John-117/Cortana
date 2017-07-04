a76080d4e3130f40f95b3594fc850285332ecf82
MasterChief-John-117 <galenguyer@outlook.com>
2017-07-03 18:54:36 -0700

    Colorful ping

--- Cortana/Modules/BotModule.cs
+++ Cortana/Modules/BotModule.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
@@ -10,10 +11,14 @@ namespace Cortana.Modules
         [Command("ping")][Summary("Gets the time to Discord's servers")]
         public async Task Ping()
         {
+            var rand = new Random();
             var watch = Stopwatch.StartNew();
             await Context.Message.ModifyAsync(msg => msg.Content = "͏");
             watch.Stop();
-            await Context.Message.ModifyAsync(msg => msg.Embed = new EmbedBuilder().WithDescription($":ping_pong: Ping took `{watch.ElapsedMilliseconds}`ms").Build());
+            await Context.Message.ModifyAsync(msg => msg.Embed = new EmbedBuilder()
+                .WithDescription($":ping_pong: Ping took `{watch.ElapsedMilliseconds}`ms")
+                .WithColor(new Color(rand.Next(255), rand.Next(255), rand.Next(255)))
+                .Build());
         }
     }
 }
