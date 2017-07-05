9bbde89046089125f5565271f32d06e1fb477290
MasterChief-John-117 <galenguyer@outlook.com>
2017-07-05 09:31:14 -0700

    Server +TextChannels +VoiceChannels

--- Cortana/Modules/ServerModule.cs
+++ Cortana/Modules/ServerModule.cs
@@ -51,13 +51,95 @@ namespace Cortana.Modules
             {
                 string msg = "";
                 msg += $"Channels for `{name}`\n";
-                msg += $"__**Text Channes**__\n";
+                msg += $"__**Text Channels**__\n";
                 msg += textChannels;
                 msg += $"__**Voice Channels**__\n";
                 msg += voiceChannels;
                 await ReplyAsync(msg);
             }
-
+        }
+        [Command("listTextChannels")]
+        [Summary("List all *text* channels in the guild")]
+        [Remarks("server")]
+        public async Task ListTextChannels(ulong id = 0)
+        {
+            IGuild guild;
+            if (id == 0) guild = Context.Guild;
+            else
+            {
+                try
+                {
+                    guild = Context.Client.GetGuildAsync(id).Result;
+                }
+                catch (Exception e)
+                {
+                    await ReplyAsync($"That doesn't appear to be a valid guild ID!");
+                    return;
+                }
+            }
+            string name = guild.Name;
+            string textChannels = "";
+            foreach (var c in guild.GetTextChannelsAsync().Result.OrderBy(c => c.Position))
+            {
+                textChannels += $"**{c.Position}:** {c.Name}\n";
+            }
+            if ((await Context.Guild.GetCurrentUserAsync()).GetPermissions(Context.Channel as IGuildChannel).EmbedLinks)
+            {
+                var em = new EmbedBuilder();
+                em.WithTitle($"Channels for {name}");
+                em.WithDescription(textChannels);
+                em.WithCurrentTimestamp();
+                await ReplyAsync("", embed: em.Build());
+            }
+            else
+            {
+                string msg = "";
+                msg += $"Text channels for `{name}`\n";
+                msg += textChannels;
+                await ReplyAsync(msg);
+            }
+        }
+        [Command("listVoiceChannels")]
+        [Summary("List all *voice* channels in the guild")]
+        [Remarks("server")]
+        public async Task ListVoiceChannels(ulong id = 0)
+        {
+            IGuild guild;
+            if (id == 0) guild = Context.Guild;
+            else
+            {
+                try
+                {
+                    guild = Context.Client.GetGuildAsync(id).Result;
+                }
+                catch (Exception e)
+                {
+                    await ReplyAsync($"That doesn't appear to be a valid guild ID!");
+                    return;
+                }
+            }
+            string name = guild.Name;
+            string voiceChannels = "";
+            foreach (var c in guild.GetVoiceChannelsAsync().Result.OrderBy(c => c.Position))
+            {
+                voiceChannels += $"**{c.Position}:** {c.Name}\n";
+            }
+            if ((await Context.Guild.GetCurrentUserAsync()).GetPermissions(Context.Channel as IGuildChannel).EmbedLinks)
+            {
+                var em = new EmbedBuilder();
+                em.WithTitle($"Voice channels for {name}");
+                em.WithDescription(voiceChannels);
+                em.WithCurrentTimestamp();
+                await ReplyAsync("", embed: em.Build());
+            }
+            else
+            {
+                string msg = "";
+                msg += $"Voice channels for `{name}`\n";
+                msg += $"__**Voice Channels**__\n";
+                msg += voiceChannels;
+                await ReplyAsync(msg);
+            }
         }
     }
 }
