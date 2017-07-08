d61f948f8f65d6d3b1425f3b259f9c328101cf31
MasterChief-John-117 <galenguyer@outlook.com>
2017-07-08 12:19:05 -0700

    +"curl" command

---
+++ Cortana/Modules/WebModule.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace Cortana.Modules
+{
+    public class WebModule : ModuleBase
+    {
+        [Command("curl")]
+        [Summary("Get a string from a URL")]
+        public async Task Curl(string url)
+        {
+            using (var client = new WebClient())
+            {
+                await ReplyAsync($"```\n{client.DownloadString(url)}```");
+            }
+        }
+    }
+}
