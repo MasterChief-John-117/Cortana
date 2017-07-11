634cd969ec376e629293f42b182f3364092a0848
MasterChief-John-117 <galenguyer@outlook.com>
2017-07-10 17:53:43 -0700

    handling command

--- Cortana/CommandHandler.cs
+++ Cortana/CommandHandler.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using Newtonsoft.Json;
 
 namespace Cortana
 {
@@ -13,6 +17,8 @@ namespace Cortana
         private DiscordSocketClient _client;
         private IServiceProvider _map;
         public static Configuration _config = new Configuration();
+
+        public List<CustomCommand> CustomCommands = JsonConvert.DeserializeObject<List<CustomCommand>>(File.ReadAllText("files/customCommands.json"));
         
         public async Task Install(IServiceProvider map)
         {
@@ -47,6 +53,17 @@ namespace Cortana
 
             // Determine if the message has a valid prefix, adjust argPos
             if (!message.HasStringPrefix(_config.Prefix, ref argPos)) return;
+
+            string cmd = parameterMessage.Content.Remove(0, _config.Prefix.Length).ToLower();
+            if (CustomCommands.Any(c => c.Command == cmd) ||
+                CustomCommands.Any(c => c.Aliases.Any(a => a == cmd)))
+            {
+                var command = CustomCommands.First(c => c.Command == cmd || c.Aliases.Any(a => a == cmd));
+                if (command.Delete) await parameterMessage.DeleteAsync();
+                await parameterMessage.Channel.SendMessageAsync(command.Value);
+                return;
+            }
+            
             // Create a Command Context
             var context = new CommandContext(_client, message);
             // Execute the Command, store the result
