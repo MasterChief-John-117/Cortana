fff2d90beb8b6cf8b493049a5c2cce8722601c4a
MasterChief-John-117 <galenguyer@outlook.com>
2017-07-10 22:37:32 -0700

    +command remove +list

--- Cortana/Modules/CustomCommandModule.cs
+++ Cortana/Modules/CustomCommandModule.cs
@@ -14,7 +14,7 @@ namespace Cortana.Modules
         [Command("add")]
         [Summary("Add a new custom command")]
         [Remarks("commands")]
-        public async Task Custom_Commands(string name, [Remainder] string text)
+        public async Task addCommand(string name, [Remainder] string text)
         {
             if (CommandHandler.CustomCommands.Any(c => c.Command == name.ToLower()) ||
                 CommandHandler.CustomCommands.Any(c => c.Aliases.Any(a => a == name.ToLower())))
@@ -25,14 +25,56 @@ namespace Cortana.Modules
             var cmd = new CustomCommand(name, text);
             CommandHandler.CustomCommands.Add(cmd);
 
-            var em = new EmbedBuilder().WithTitle("A new command has been added!").WithDescription("");
+            var em = new EmbedBuilder();
             em.AddField(new EmbedFieldBuilder().WithName("Name").WithValue(cmd.Command).WithIsInline(true));
             em.AddField(new EmbedFieldBuilder().WithName("Aliases").WithValue(cmd.GetAliases()).WithIsInline(true));
             em.AddField(new EmbedFieldBuilder().WithName("Value").WithValue(cmd.Value).WithIsInline(false));
             em.AddField(new EmbedFieldBuilder().WithName("Delete").WithValue(cmd.Delete).WithIsInline(true));
             File.WriteAllText("files/customCommands.json", JsonConvert.SerializeObject(CommandHandler.CustomCommands, Formatting.Indented));
 
-            await ReplyAsync("", embed: em.Build());
+            await ReplyAsync($"The command `{cmd.Command}` has been created!", embed: em.Build());
         }
+        [Command("remove")][Alias("delete")]
+        [Summary("removes a custom command")]
+        [Remarks("commands")]
+        public async Task RemoveCommand(string name)
+        {
+            if (CommandHandler.CustomCommands.All(c => c.Command != name.ToLower()))
+            {
+                await ReplyAsync($"There is no command named `{name}`");
+                return;
+            }
+            var cmd = CommandHandler.CustomCommands.First(c => c.Command.Equals(name.ToLower()));
+            CommandHandler.CustomCommands.Remove(cmd);
+            
+            File.WriteAllText("files/customCommands.json", JsonConvert.SerializeObject(CommandHandler.CustomCommands, Formatting.Indented));
+
+            await ReplyAsync($"The command `{name}`has been removed");
+        }
+        [Command("list")]
+        public async Task Custom_List()
+        {
+            string msg = "Custom Commands: \n```\n";
+            int i = 1;
+            foreach (var command in CommandHandler.CustomCommands)
+            {
+                if (i % 4 == 0 && i != 0) msg += command.Command + "\n";
+                else
+                {
+                    msg += command.Command;
+                    for (int j = command.Command.Length; j < 15; j++) msg += " ";
+                }
+                i++;
+                if (msg.Length > 1500)
+                {
+                    msg += $"{command.Command}\n```";
+                    await ReplyAsync(msg);
+                    msg = "```\n";
+                }
+            }
+            await ReplyAsync(msg + "\n```");
+            await Context.Message.DeleteAsync();
+        }
+
     }
 }
