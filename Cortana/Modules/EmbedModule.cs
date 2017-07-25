7aff047e3e37677fa9b6269b177d894e485f7a7d
MasterChief-John-117 <galenguyer@outlook.com>
2017-07-25 13:12:46 -0700

    embedText + embedImage

---
+++ Cortana/Modules/EmbedModule.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Cortana.Utilities;
+using Discord;
+using Discord.Commands;
+using Color = Discord.Color;
+
+namespace Cortana.Modules
+{
+    public class EmbedModule : ModuleBase
+    {
+        [Command("embedText")]
+        [Alias("em")]
+        public async Task embedText([Remainder] string input)
+        {
+            await Context.Message.DeleteAsync();
+            string title = Regex.Match(input, @"\[([^)]*)\]").Groups[1].Value;
+            string text = Regex.Match(input, @"\(([^)]*)\)").Groups[1].Value;
+
+            var em = new EmbedBuilder();
+            em.WithDescription(text);
+            em.WithTitle(title);
+            em.WithColor(new Color(240, 71, 71));
+            
+            await ReplyAsync("", embed: em.Build());
+        }
+
+        [Command("embedImage")]
+        [Alias("img")]
+        public async Task EmbedImage([Remainder] string input)
+        {
+            var watch = new Stopwatch();
+            watch.Restart();
+            await Context.Message.DeleteAsync();
+            string title = Regex.Match(input, @"\[([^)]*)\]").Groups[1].Value;
+            string link = Regex.Match(input, @"\(([^)]*)\)").Groups[1].Value;
+            var cmd = new CommandHandler();
+        
+            new WebClient().DownloadFile(new Uri(link), "files/tempImg.png");
+            using (Bitmap bitmap = new Bitmap("files/tempImg.png"))
+            {
+                var clr = new ImageUtils().GetAverageColor(bitmap, cmd, 10);
+
+                var em = new EmbedBuilder()
+                    .WithTitle(title)
+                    .WithImageUrl(link)
+                    .WithColor(new Discord.Color(clr.R, clr.G, clr.B))
+                    .WithFooter(new EmbedFooterBuilder().WithText(
+                        $"{cmd.Iterator} cycles | {watch.ElapsedMilliseconds}ms " +
+                        $"| {new FileInfo(("files/tempImg.png")).Length / 1024}kb"))
+                    .Build();
+                await ReplyAsync("", embed: em);
+            }
+        }
+    }
+}
