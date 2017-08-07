07bb90920c074e0762cd4f5d016f0195051532ab
MasterChief-John-117 <galenguyer@outlook.com>
2017-08-07 07:25:58 -0700

    +UrbanDictionaryClient

---
+++ UrbanDictionary/UrbanClient.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace UrbanDictionary
+{
+    public class UrbanClient
+    {
+        public UrbanClient()
+        {
+            
+        }
+
+        public UrbanClientResponse GetClientResponse(string rawWord)
+        {
+            string word = SanitizeQuery(rawWord);
+            return new UrbanClientResponse(word);   
+        }
+
+        public string GetFirstDefinition(UrbanClientResponse response)
+        {
+            if (response.ResultType.Equals("no_results"))
+            {
+                return "No definition found";
+            }
+            else return response.List.First().Definition;
+        }
+        
+        private string SanitizeQuery(string initial)
+        {
+            string noSpecial = new string(initial.ToCharArray().Where(c => char.IsLetter(c) || c == ' ').ToArray());
+            return noSpecial.Replace(' ', '+');
+        }
+    }
+}
---
+++ UrbanDictionary/UrbanClientResponse.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace UrbanDictionary
+{
+    public class UrbanClientResponse
+    {
+        [JsonProperty("tags")]
+        public List<string> Tags = new List<string>();
+        [JsonProperty("result_type")]
+        public string ResultType;
+        [JsonProperty("list")]
+        public List<UrbanResult> List = new List<UrbanResult>();
+        [JsonProperty("sounds")]
+        public List<string> Sounds = new List<string>();
+
+        [JsonConstructor]
+        public UrbanClientResponse()
+        {
+            
+        }
+
+        public UrbanClientResponse(string word)
+        {
+            string responseString;
+            using (var client = new WebClient())
+            {
+                responseString = client.DownloadStringTaskAsync(new Uri($"http://api.urbandictionary.com/v0/define?term={word}")).Result;
+            }
+
+            UrbanClientResponse tempResponse = JsonConvert.DeserializeObject<UrbanClientResponse>(responseString);
+
+            Tags = tempResponse.Tags;
+            ResultType = tempResponse.ResultType;
+            List = tempResponse.List;
+            Sounds = tempResponse.Sounds;
+        }
+    }
+}
