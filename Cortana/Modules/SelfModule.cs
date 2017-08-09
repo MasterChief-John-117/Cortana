03736a205e0a5c2a61f26eff754937c9044c6254
MasterChief-John-117 <galenguyer@outlook.com>
2017-08-09 09:09:02 -0700

    SetGame

---
+++ Cortana/Modules/SelfModule.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Cortana.Modules
+{
+    public class SelfModule : ModuleBase
+    {
+        [Command("game")]
+        public async Task SetGame([Optional, Remainder] string gameToSet)
+        {
+            await Context.Message.DeleteAsync();
+            if (!string.IsNullOrEmpty(gameToSet))
+            {
+                await (Context.Client as DiscordSocketClient).SetGameAsync(gameToSet);
+                await ReplyAsync($"Game has been set to **{gameToSet}**");
+            }
+            else
+            {
+                await (Context.Client as DiscordSocketClient).SetGameAsync("");
+                await ReplyAsync("Game has been cleared");
+            }
+    }
+    }
+}
