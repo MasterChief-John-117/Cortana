d7e3a14dd95e8ad8c98bd9d7ebdf441becb14250
MasterChief-John-117 <galenguyer@outlook.com>
2017-08-14 21:18:29 -0700

    reply to message in channel

--- Cortana/Modules/ChatModule.cs
+++ Cortana/Modules/ChatModule.cs
@@ -38,6 +38,54 @@ namespace Cortana.Modules
             await msg.DeleteAsync();
         }
 
+        [Command("reply")]
+        public async Task ReplyToMessage(ulong messageID, [Remainder] string reply)
+        {
+            await Context.Message.DeleteAsync();
+            string finalMessage = "";
+            var channel = Context.Channel as IMessageChannel;
+
+            var msgs = (channel as IMessageChannel).GetMessagesAsync().Flatten().Result;
+
+            for (int i = 0; i < 10; i++)
+            {
+                var newmsgs = (channel as IMessageChannel).GetMessagesAsync(msgs.Last(), Direction.Before).Flatten().Result;
+                msgs = msgs.Concat(newmsgs);
+                if (msgs.Any(m => m.Id.Equals(messageID))) break;
+                else
+                {
+                    await ReplyAsync("The requested message could not be found");
+                    return;
+                }
+            }
+            if (!msgs.Any(m => m.Id.Equals(messageID)))
+            {            
+                await ReplyAsync("The requested message could not be found");
+                return;
+            }
+
+            var replyToMessage = msgs.First(m => m.Id.Equals(messageID));
+
+            new WebClient().DownloadFile(new Uri(replyToMessage.Author.GetAvatarUrl()), "files/tempImg.png");
+
+            System.Drawing.Color clr;
+
+            using (Bitmap bitmap = new Bitmap("files/tempImg.png"))
+            {
+                clr = new ImageUtils().GetAverageColor(bitmap, new CommandHandler(), 10);
+            }
+
+            var emb = new EmbedBuilder()
+            .WithAuthor(new EmbedAuthorBuilder()
+                .WithIconUrl(replyToMessage.Author.GetAvatarUrl())
+                .WithName(replyToMessage.Author.ToString()))
+            .WithDescription(replyToMessage.Content)
+            .WithColor(new Discord.Color(clr.R, clr.G, clr.B))
+            .WithFooter(new EmbedFooterBuilder().WithText(replyToMessage.CreatedAt.ToString()));
+
+            await ReplyAsync("", embed: emb.Build());
+            await ReplyAsync(reply);
+        }
         [Command("markov")][Summary("Markov chain generator for the channel\nOptions: [messages to grab] {minimum word count}")]
         public async Task ChatMarkov([Remainder] string options = "[300] {5}")
         {
