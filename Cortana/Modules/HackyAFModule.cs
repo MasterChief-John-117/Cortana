9254330c1a708c9ea72b33c2104c04e60013357b
MasterChief-John-117 <galenguyer@outlook.com>
2017-08-17 23:51:11 -0700

    small fixes for HackyAf stuff

--- Cortana/Modules/HackyAFModule.cs
+++ Cortana/Modules/HackyAFModule.cs
@@ -11,23 +11,30 @@ namespace Cortana.Modules
     public class HackyAFModule : ModuleBase
     {
         [Command("suppressRoleMentions")]
-        [Summary("Supresses role mentions for a given guild")][Remarks("no-help")]
+        [Summary("Supresses role mentions for a given guild")]
+        [Remarks("no-help")]
         public async Task AddGuildRoleSuppression(ulong guildId)
         {
             var guild = Context.Client.GetGuildAsync(guildId).Result;
 
-            if(!CommandHandler._config.GuildSuppressionList.Contains(guild.Id))CommandHandler._config.GuildSuppressionList.Add(guild.Id);
+            if (!CommandHandler._config.GuildSuppressionList.Contains(guild.Id))
+            {
+                CommandHandler._config.GuildSuppressionList.Add(guild.Id);
+            }
             File.WriteAllText("files/config.json", JsonConvert.SerializeObject(CommandHandler._config, Formatting.Indented));
             await ReplyAsync($"{guild.Name} has been added to the Role Mention Suppression list");
         }
         [Command("removesuppressRoleMentions")]
-        [Summary("Supresses role mentions for a given guild")]
+        [Summary("un-suprresses role mentions for a given guild")]
         [Remarks("no-help")]
         public async Task RemoveGuildRoleSuppression(ulong guildId)
         {
             var guild = Context.Client.GetGuildAsync(guildId).Result;
 
-            if(CommandHandler._config.GuildSuppressionList.Contains(guild.Id))CommandHandler._config.GuildSuppressionList.Remove(guild.Id);
+            if (CommandHandler._config.GuildSuppressionList.Contains(guild.Id))
+            {
+                CommandHandler._config.GuildSuppressionList.Remove(guild.Id);
+            }
             File.WriteAllText("files/config.json", JsonConvert.SerializeObject(CommandHandler._config, Formatting.Indented));
             await ReplyAsync($"{guild.Name} has been removed from the Role Mention Suppression list");
         }
